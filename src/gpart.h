c0015ad5e56df8bc20e88ee5074e9f3e6e3d849d
sfanxiang <sfanxiang@gmail.com>
2017-12-31 22:19:40 +0800

    add support for Haiku

--- src/gpart.h
+++ src/gpart.h
@@ -26,6 +26,36 @@
 
 typedef uint8_t byte_t;
 
+#ifdef __HAIKU__
+#	if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#		define htole16(x) (x)
+#		define htobe16(x) __builtin_bswap16(x)
+#		define htole32(x) (x)
+#		define htobe32(x) __builtin_bswap32(x)
+#		define htole64(x) (x)
+#		define htobe64(x) __builtin_bswap64(x)
+#		define le16toh(x) (x)
+#		define be16toh(x) __builtin_bswap16(x)
+#		define le32toh(x) (x)
+#		define be32toh(x) __builtin_bswap32(x)
+#		define le64toh(x) (x)
+#		define be64toh(x) __builtin_bswap64(x)
+#	else
+#		define htole16(x) __builtin_bswap16(x)
+#		define htobe16(x) (x)
+#		define htole32(x) __builtin_bswap32(x)
+#		define htobe32(x) (x)
+#		define htole64(x) __builtin_bswap64(x)
+#		define htobe64(x) (x)
+#		define le16toh(x) __builtin_bswap16(x)
+#		define be16toh(x) (x)
+#		define le32toh(x) __builtin_bswap32(x)
+#		define be32toh(x) (x)
+#		define le64toh(x) __builtin_bswap64(x)
+#		define be64toh(x) (x)
+#	endif
+#endif
+
 #define le16(x) htole16(x)
 #define be16(x) htobe16(x)
 #define le32(x) htole32(x)
